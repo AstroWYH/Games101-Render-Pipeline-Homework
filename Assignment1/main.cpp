461351c95e1ddd238b2eb06eed21ed8d85c37a96
wangyuhang <819369336@qq.com>
2022-04-22 02:34:57 +0800

    [add] Games101软渲染管线作业-Assignment1

---
+++ Assignment1/main.cpp
@@ -0,0 +1,146 @@
+#include "Triangle.hpp"
+#include "rasterizer.hpp"
+#include <eigen3/Eigen/Eigen>
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+constexpr double MY_PI = 3.1415926;
+
+Eigen::Matrix4f get_view_matrix(Eigen::Vector3f eye_pos)
+{
+    Eigen::Matrix4f view = Eigen::Matrix4f::Identity();
+
+    Eigen::Matrix4f translate;
+    translate << 1, 0, 0, -eye_pos[0], 0, 1, 0, -eye_pos[1], 0, 0, 1,
+        -eye_pos[2], 0, 0, 0, 1;
+
+    view = translate * view;
+
+    return view;
+}
+
+Eigen::Matrix4f get_model_matrix(float rotation_angle)
+{
+    Eigen::Matrix4f model = Eigen::Matrix4f::Identity();
+
+    // TODO: Implement this function
+    // Create the model matrix for rotating the triangle around the Z axis.
+    // Then return it.
+
+    model << std::cos(rotation_angle), -std::sin(rotation_angle), 0, 0, // wyh 模型变换 模型空间->世界空间
+        std::sin(rotation_angle), std::cos(rotation_angle), 0, 0,
+        0, 0, 1, 0,
+        0, 0, 0, 1;
+    // std::cout << "wyh model" << std::endl;
+
+    return model;
+}
+
+Eigen::Matrix4f get_projection_matrix(float eye_fov, float aspect_ratio,
+                                      float zNear, float zFar)
+{
+    // Students will implement this function
+    // wyh projection = Mortho * Mpersp2ortho
+    // wyh Mortho = Mr * Mt
+    Eigen::Matrix4f projection = Eigen::Matrix4f::Identity();
+
+    // TODO: Implement this function
+    // Create the projection matrix for the given parameters.
+    // Then return it.
+
+    Eigen::Matrix4f Mpersp2ortho = Eigen::Matrix4f::Identity();
+    Eigen::Matrix4f Mortho = Eigen::Matrix4f::Identity();
+    Eigen::Matrix4f Mr = Eigen::Matrix4f::Identity();
+    Eigen::Matrix4f Mt = Eigen::Matrix4f::Identity();
+
+    Mr << 2/(2*((std::tan(eye_fov/2)/10)*aspect_ratio)), 0, 0, 0,
+        0, 2/(2*(std::tan(eye_fov/2)/10)), 0, 0,
+        0, 0, 2/(zNear-zFar), 0,
+        0, 0, 0, 1;
+
+    Mortho = Mr * Mt; // wyh Mortho: (正交)投影矩阵 归一化正交投影矩阵 缩放 * 平移 (此题位于中心, 无需平移, 即Mt为I)
+
+    Mpersp2ortho << zNear, 0, 0, 0, // wyh Mpersp2ortho: (透视->正交)变换矩阵
+        0, zNear, 0, 0,
+        0, 0, zNear+zFar, -zNear*zFar,
+        0, 0, 1, 0;
+
+    projection = Mortho * Mpersp2ortho; // wyh (透视)投影矩阵 = (正交)投影矩阵 * (透视->正交)变换矩阵
+    // projection = Mpersp2ortho; // wyh 若用(正交)投影矩阵当作(透视)投影矩阵, 结果是不对的
+    // std::cout << "wyh projection" << std::endl;
+
+    return projection;
+}
+
+int main(int argc, const char** argv)
+{
+    float angle = 0;
+    bool command_line = false;
+    std::string filename = "output.png";
+
+    if (argc >= 3) {
+        command_line = true;
+        angle = std::stof(argv[2]); // -r by default
+        if (argc == 4) {
+            filename = std::string(argv[3]);
+        }
+        else
+            return 0;
+    }
+
+    rst::rasterizer r(700, 700);
+
+    Eigen::Vector3f eye_pos = {0, 0, 5};
+
+    std::vector<Eigen::Vector3f> pos{{2, 0, -2}, {0, 2, -2}, {-2, 0, -2}}; // wyh 三角形的空间坐标
+
+    std::vector<Eigen::Vector3i> ind{{0, 1, 2}}; // wyh 索引
+
+    auto pos_id = r.load_positions(pos);
+    auto ind_id = r.load_indices(ind);
+
+    int key = 0;
+    int frame_count = 0;
+
+    if (command_line) {
+        r.clear(rst::Buffers::Color | rst::Buffers::Depth);
+
+        r.set_model(get_model_matrix(angle));
+        r.set_view(get_view_matrix(eye_pos));
+        r.set_projection(get_projection_matrix(45, 1, 0.1, 50));
+
+        r.draw(pos_id, ind_id, rst::Primitive::Triangle);
+        cv::Mat image(700, 700, CV_32FC3, r.frame_buffer().data());
+        image.convertTo(image, CV_8UC3, 1.0f);
+
+        cv::imwrite(filename, image);
+
+        return 0;
+    }
+
+    while (key != 27) {
+        r.clear(rst::Buffers::Color | rst::Buffers::Depth);
+
+        r.set_model(get_model_matrix(angle));
+        r.set_view(get_view_matrix(eye_pos));
+        r.set_projection(get_projection_matrix(45, 1, 0.1, 50));
+
+        r.draw(pos_id, ind_id, rst::Primitive::Triangle);
+
+        cv::Mat image(700, 700, CV_32FC3, r.frame_buffer().data());
+        image.convertTo(image, CV_8UC3, 1.0f);
+        cv::imshow("image", image);
+        key = cv::waitKey(10); // wyh opencv等待用户触发
+
+        std::cout << "frame count: " << frame_count++ << '\n';
+
+        if (key == 'a') {
+            angle += MY_PI/180;
+        }
+        else if (key == 'd') {
+            angle -= MY_PI/180;
+        }
+    }
+
+    return 0;
+}
